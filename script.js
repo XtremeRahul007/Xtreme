80f488d0a2564b8d0713a15554c976e33b219c5c
XtremeRahul007 <rahulmanna0011@gmail.com>
2025-10-29 08:41:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const toggler = document.querySelector('.navbar-toggler');
+    const collapse = document.querySelector('.navbar-collapse');
+
+    if (toggler && collapse) {
+        toggler.addEventListener('click', function () {
+            collapse.classList.toggle('show');
+        });
+    }
+});
